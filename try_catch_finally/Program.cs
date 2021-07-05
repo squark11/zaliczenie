e81b091eec0dde2c213f79f2ad311d6b2b4f1ba4
squark <squark@KACPER-LAPTOP>
2021-07-05 23:31:53 +0200

    Dodaj pliki projektów.

---
+++ try_catch_finally/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for two numbers, x and y
+            Console.WriteLine("Enter x:");
+            int.TryParse(Console.ReadLine(), out int x);
+            Console.WriteLine("Enter y:");
+            int.TryParse(Console.ReadLine(), out int y);
+            // Try dividing x by y - catch exceptions when it goes wrong
+            try
+            {
+                int result = x / y;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch // or catch (Exception) or catch (Exception ex)
+            {
+                Console.WriteLine("Some general exception!");
+            }
+            finally
+            {
+                Console.WriteLine("This is always executed. Closing the app.");
+            }
+        }
+    }
+}
+
+
